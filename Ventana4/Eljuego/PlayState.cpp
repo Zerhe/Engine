dc977f7c54e21fce8136ff9beb0e77eaccbe0b2a
Zerhe <dante_ro@hotmail.com.ar>
2017-06-19 17:05:22 -0300

    AnimacionEcha

--- Ventana4/Eljuego/PlayState.cpp
+++ Ventana4/Eljuego/PlayState.cpp
@@ -20,42 +20,20 @@ bool PlayState::onShutdown()
 bool PlayState::onUpdate() 
 {
 	_contador++;
-	switch (_contador)
+	if (_contador == 20)
 	{
-		case 20:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 0, 10);
-			break;
-		case 40:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 1, 10);
-			break;
-		case 60:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 2, 10);
-			break;
-		case 80:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 3, 10);
-			break;
-		case 100:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 4, 10);
-			break;
-		case 120:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 5, 10);
-			break;
-		case 140:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 6, 10);
-			break;
-		case 160:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 7, 10);
-			break;
-		case 180:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 8, 10);
-			break;
-		case 200:
-			_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, 9, 10);
-			_contador = 0;
-			break;
+		_sprite->setSize(800.0f, 200.0f, 200.0f, 312.0f, 2000.0f, 312.0f, _frame, 10);
+		_contador = 0;
+		_frame++;
 	}
+	
 	_sprite->_x--;
 	if (_sprite->_x < -1000)
 		_sprite->_x = 800;
+
+	if (_frame == 10)
+	{
+		_frame = 0;
+	}
 	return true;
 }
